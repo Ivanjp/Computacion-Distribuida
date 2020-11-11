756d5b7ab5e6dfefbbcda1b2879979930eb21dc3
Ivanjp <jorgeperez@ciencias.unam.mx>
2020-11-11 02:23:22 -0600

    Se agregaron otras funciones

--- Practica 2/chat.py
+++ Practica 2/chat.py
@@ -2,6 +2,7 @@ import time
 import socket
 import threading
 import sys
+import os
 
 class Chat:
 
@@ -14,20 +15,25 @@ class Chat:
         self.nickname_chat = "Noobmaster69"
         self.nickname_vecino = "JohnConnor3"
         self.puerto_s = puerto
-        self.listaC = ["jejei 2920" , "juds 4536"]
+        self.listaC = ["PepperPots 2920" , "AMLO 4536"]
+        self.puerto_v = ""
+        self.flagList = False
 
     def conecta_cliente(self,puerto,op):
         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         server_Adress = ('localhost',puerto)
-        sock.connect(server_Adress)
-        self.conexion_cliente = sock
-        if op:
-            cadena = str(self.puerto_s) + " " + self.nickname_chat
-            self.conexion_cliente.sendall(cadena.encode())
-        else: 
-            self.conexion_cliente.sendall(("@" + self.nickname_chat).encode())
-        
-        self.ch_client = True
+        try:
+            sock.connect(server_Adress)
+            self.conexion_cliente = sock
+            if op:
+                cadena = str(self.puerto_s) + " " + self.nickname_chat
+                self.conexion_cliente.sendall(cadena.encode())
+            else: 
+                self.conexion_cliente.sendall(("@" + self.nickname_chat).encode())
+            
+            self.ch_client = True
+        except:
+            print("Canal no activo, intenta mas tarde")    
 
     def espera_servidor(self,op):
         if op:
@@ -36,6 +42,7 @@ class Chat:
             self.socket_server.bind(server_Address)
             self.socket_server.listen(1)
             print("> Escuchando en el puerto " + str(self.puerto_s))
+            print("> Tu nombre de usuario por defecto es "+ self.nickname_chat + " para cambiarlo escribe @chname seguido del nombre que quieres usar" )
 
         self.conexion_servidor, server_Address = self.socket_server.accept()
 
@@ -43,6 +50,7 @@ class Chat:
             try:
                 respuesta = self.conexion_servidor.recv(100).decode("utf-8")
                 puerto = int(respuesta.split(" ")[0])
+                self.puerto_v = str(puerto)
                 self.nickname_vecino = respuesta.split(" ")[1]
                 print("> Sincronizando en puerto " + str(puerto))
                 self.conecta_cliente(puerto,False)
@@ -52,44 +60,67 @@ class Chat:
         self.escucha()
 
     def entrada(self):
-        print("> Listo para recibir comandos o mensajes")
+        print("> Bienvenido de nuevo")
         while True:
             comando = input()
-            if comando.startswith("@sobrenombre"):
+            if comando.startswith("@chname"):
                 if not self.ch_client and not self.ch_server:
                     self.nickname_chat = comando.split(" ")[1]
-                    print("> El sobrenombre a cambiado a " + self.nickname_chat)
+                    print("> Nombre de usuario cambiado a " + self.nickname_chat)
                 else:
-                    print("No fue posible cambiar el sobrenombre durante la ocnversacion")
+                    print("NO puedes cambiar de nombre mientras chateas")
             elif comando.startswith("@conecta"):
                 if not self.ch_client and not self.ch_server:
-                    puerto = int(comando.split(" ")[1])
-                    self.conecta_cliente(puerto, True)
-                    self.listaC.append(self.nickname_vecino + " " + str(puerto))
+
+                    p = ""
+                    for n in self.listaC:
+                        if comando.split(" ")[1] == n.split(" ")[0]:
+                            self.flagList = True
+                            p = n
+                    
+                    if self.flagList == True:
+                        puerto = int(p.split(" ")[1])
+                        self.puerto_v = str(puerto)
+                        self.conecta_cliente(puerto, True)
+                    elif self.flagList == False and any(chr.isdigit() for chr in comando.split(" ")[1]) == False:
+                        print("> Este usuario no esta en tu lista de contactos, ingresa un usuario que tengas agregado o el puerto de otro usuario")
+                    elif self.flagList == False and any(chr.isdigit() for chr in comando.split(" ")[1]) == True:
+                        puerto = int(comando.split(" ")[1])
+                        self.puerto_v = str(puerto)
+                        self.conecta_cliente(puerto, True)  
+
                 else:
                     print("Ya hay un chat activo, escribe @desconecta para terminar")
             elif comando.startswith("@contactos"):
-                print("Lista de contactos:\n")
-                for x in self.listaC:
-                    print(str(self.listaC.index(x)+1)+". "+x)
+                if not self.ch_client and not self.ch_server:
+                    print("Lista de contactos:\n")
+                    for x in self.listaC:
+                        print(str(self.listaC.index(x)+1)+". "+x)
+                else:
+                    print("NO puedes consultar tu lista de contactos mientras chateas")
             elif comando.startswith("@desconecta"):
                 if not self.ch_client and not self.ch_server:
                     print("> No hay nunguna conexion")
                     continue
                 print("> Saliste de la conexion") 
+                
+                if self.flagList == False:
+                    self.listaC.append(self.nickname_vecino+" "+ str(self.puerto_v))
+
                 self.ch_server = False
                 self.ch_client = False
+                self.flagList = False
                 self.conexion_cliente.sendall(comando.encode())
                 self.conexion_servidor.close()
                 self.conexion_cliente.close()
                 threading.Thread(target=self.espera_servidor,args=(False,)).start()
             elif comando.startswith("@salir"):
-                print("> Hasta luego")    
-                try:
-                    self.conexion_servidor.close()
-                    sys.exit()
-                except:
-                    pass
+                if not self.ch_client and not self.ch_server:
+                    print("> Hasta luego") 
+                    self.socket_server.close()
+                    os._exit(1)
+                else:
+                    print("NO puedes cerrar la aplicacion hasta desconectarte del chat, escribe @desconecta.")
             elif comando.startswith("@"):
                 print("> Comando no soportado")
             else: 
@@ -113,16 +144,26 @@ class Chat:
                 self.ch_server = False
                 self.ch_client = False
                 print("> " + self.nickname_vecino + " cerro la conversacion")
+
+                for n in self.listaC:
+                    if self.nickname_vecino in n:
+                        self.flagList = True
+                
+                if self.flagList == False:
+                 self.listaC.append(self.nickname_vecino+" "+ str(self.puerto_v))
+
                 threading.Thread(target=self.espera_servidor,args=(False,)).start()
+                self.flagList = False
                 break
             elif info.startswith("@"):
                 self.nickname_vecino = info[1:]
                 continue
-            print("[" + self.nickname_vecino + "ppp]" + info)
+            print("[" + self.nickname_vecino + "]" + info)
 
-    def corre(self):
+    def run(self):
         threading.Thread(target=self.espera_servidor,args=(True,)).start()
         self.entrada()
+                        
 
-Chat(int(sys.argv[1])).corre()                
+Chat(int(sys.argv[1])).run()                
                                     
